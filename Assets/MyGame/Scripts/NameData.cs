74a4d86d3094c299c762c9742e394d8ef8d6f293
smeerws <hello@smeerws.at>
2019-02-06 07:29:30 +0100

    init project

---
+++ Assets/MyGame/Scripts/NameData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NameData : ScriptableObject {
+
+    public string name;
+
+
+
+}
