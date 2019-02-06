43451922b62bfc0c346da8cf2fb77cb7d3adcc9c
smeerws <hello@smeerws.at>
2019-02-06 07:56:33 +0100

    updates for persistent singleton

---
+++ Assets/MyGame/Scripts/PersistentSingleton.cs
@@ -0,0 +1,42 @@
+﻿/* persistent singleton 
+ * this fixes the persistency problem (across scenes)
+ */
+using UnityEngine;
+
+public class PersistentSingleton : MonoBehaviour {
+
+    private readonly string Tag = "persistentSingleton";
+    public static PersistentSingleton instance;
+
+    private string yourName = "";
+
+    public string Name
+    {
+        get
+        {
+            Debug.Log(Tag + " get name " + yourName);
+            return yourName;
+        }
+
+        set
+        {
+            Debug.Log(Tag + " name set to " + yourName);
+            yourName = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Destroy " + Tag + " " + gameObject.name);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("Assign this " + Tag + " instance  with name " + gameObject.name);
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
